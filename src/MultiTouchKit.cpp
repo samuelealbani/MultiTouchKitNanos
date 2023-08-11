7432c9a625b043e560566fd239081ed8fe8d9afa
samuele <samuelealbani@gmail.com>
2023-08-11 18:45:59 +0100

    Add Arduino Nano Board

--- src/MultiTouchKit.cpp
+++ src/MultiTouchKit.cpp
@@ -130,6 +130,20 @@ void MultiTouchKit::setupPWM(){
         OCR2B = 0;
     
         analogReference(INTERNAL);         //change reference to 1.1V
+
+    #elif defined(ARDUINO_AVR_NANO)
+        define BOARD "Nano"
+        ////------------- Setup for Arduino Uno ---------
+        pinMode(3, OUTPUT);      // Timer02 output pin
+    
+        //Set frequency to 4MHz, 25% duty cycle
+        TCCR2A = _BV(COM2A0) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+        TCCR2B = _BV(WGM22) | _BV(CS20);
+        OCR2A = 3;
+        OCR2B = 0;
+    
+        analogReference(INTERNAL);         //change reference to 1.1V
+
     
     #elif defined(ARDUINO_AVR_LILYPAD)
         #define BOARD "Lilypad"
